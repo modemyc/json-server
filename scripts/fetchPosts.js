c31e747d2c9eaf04f60b2b06437a3544430cadae
Konstantin <1604177123@mail.ru>
2025-03-23 03:12:07 +0300

    work in progress (WIP)

---
+++ scripts/fetchPosts.js
@@ -0,0 +1,5 @@
+const loadForm = document.querySelector('.load-post-form');
+const postIdElement = document.querySelector('#post-id');
+const resultField = document.querySelector('.result');
+
+// старт json server npx json-server db.json5
